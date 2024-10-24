429ca4fc48b6bd36e5fa3002425d0e523c927f8b
Drew Nutter <dnut@users.noreply.github.com>
2024-10-24 17:11:05 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+Build and use LMDB in zig.
+
+# Build Dependencies
+
+[Zig `0.13`](https://ziglang.org/download/)
+
+# Usage
+
+Supported use cases:
+- [⬇️](#build-lmdb) Build a LMDB static library using the zig build system.
+- [⬇️](#import-lmdb-c-api-in-a-zig-project) Use the LMDB C API through auto-generated Zig bindings.
+
+## Build LMDB
+Clone this repository, then run `zig build`. 
+
+You will find a statically linked `lmdb` archive in `zig-out/lib/liblmdb.a`.
+
+You can use this with any language or build system.
+
+## Import LMDB in a Zig project
+
+Fetch `lmdb` and save it to your `build.zig.zon`:
+```
+$ zig fetch --save=lmdb https://github.com/Syndica/lmdb-zig/archive/<COMMIT_HASH>.tar.gz
+```
+
+Add the import to a module:
+```zig
+const lmdb = b.dependency("lmdb", .{}).module("lmdb");
+exe.root_module.addImport("lmdb", lmdb);
+```
+
+Import the `lmdb` module.
+```zig
+const lmdb = @import("lmdb");
+```
+
+## Test / Example
+
+For a unit test demonstrating basic usage of lmdb, see `test.zig`. Run the test with `zig build test`.
---
+++ build.zig
@@ -0,0 +1,52 @@
+const std = @import("std");
+const Build = std.Build;
+const ResolvedTarget = Build.ResolvedTarget;
+const OptimizeMode = std.builtin.OptimizeMode;
+
+pub fn build(b: *Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    // get c library
+    const lmdb_dep = b.dependency("lmdb", .{});
+    const lmdb_path = lmdb_dep.path("libraries/liblmdb");
+
+    // expose c library as zig library
+    const translate_c = b.addTranslateC(.{
+        .root_source_file = lmdb_path.path(b, "lmdb.h"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const mod = b.addModule("lmdb", .{
+        .root_source_file = translate_c.getOutput(),
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+
+    // build c library
+    const liblmdb_a = b.addStaticLibrary(.{
+        .name = "lmdb",
+        .target = target,
+        .optimize = optimize,
+    });
+    liblmdb_a.linkLibC();
+    liblmdb_a.addIncludePath(lmdb_path);
+    liblmdb_a.addCSourceFiles(.{
+        .root = lmdb_path,
+        .files = &.{ "mdb.c", "midl.c" },
+    });
+    b.installArtifact(liblmdb_a);
+    mod.addIncludePath(lmdb_path);
+    mod.linkLibrary(liblmdb_a);
+
+    // test c library in zig
+    const test_step = b.step("test", "Run tests");
+    const tests = b.addTest(.{
+        .target = target,
+        .optimize = optimize,
+        .root_source_file = b.path("test.zig"),
+    });
+    tests.root_module.addImport("lmdb", mod);
+    test_step.dependOn(&b.addRunArtifact(tests).step);
+}
